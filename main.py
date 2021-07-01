5f2ef9390fb4d8fc1fc7b1f151c7d098545819e7
Ash-born <chabchiw@gmail.com>
2021-07-01 11:10:08 +0100

    Add a Command to count days

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import discord
 from discord.ext import commands
 import os
+import datetime
 
-client = commands.Bot(command_prefix="utils", help_command=None)
-
-@client.event
-async def on_ready():
-    print('Connected')
-PREFIX = "utils "
-
+PREFIX = "utils"
+client = commands.Bot(command_prefix=PREFIX, help_command=None)
 
 
 @client.event
 async def on_ready():
     print("Bot ready")
-    await client.change_presence(status=discord.Status.online, activity=discord.Game(f"{PREFIX}help | Bot à multiple fonctions"))
+    await client.change_presence(status=discord.Status.online,
+                                 activity=discord.Game(f"{PREFIX} help | Bot à multiple fonctions"))
 
+
+@client.event
+async def on_message(message):
+    if message.content == "day":
+        today = datetime.date.today()
+        last = datetime.date(2021, 4, 24)
+        diff = today - last
+        await message.channel.send( f"Days : {diff.days}")
 
 client.run(os.getenv("TOKEN"))
