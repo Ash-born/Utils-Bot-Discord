85dadcc410e7ddc4216a5bf45697c7b891c13ead
MarronEyes <marounyeux@gmail.com>
2021-12-30 13:39:33 +0100

    Create help command in bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,6 @@
+import asyncio
+import io
+import sys
 import logging
 import os
 import discord
@@ -10,6 +13,12 @@ logger = logging.getLogger(__name__)
 class Bot(commands.Bot):
     instance = None
     FORMAT = "[%(asctime)s](%(name)s): %(message)s"
+    MAX_COGS_HELP = 6
+    MAX_COMMANDS_HELP = 8
+    debuggers = [
+        640847986292949012,
+        326806932566310922
+    ]
 
     def __init__(self, args=None):
         self.args = args
@@ -17,11 +26,109 @@ class Bot(commands.Bot):
         self.PREFIX = None
         self.TOKEN = None
         self.DEBUG = None
+        self.logs = io.StringIO()
         self.process_args()
 
         Bot.instance = self
         intents = discord.Intents.all()
         super().__init__(command_prefix=self.PREFIX, help_command=None, intents=intents)
+        self.add_command(commands.Command(self.help, name="help"))
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        logger.info("Bot ready to use.")
+        game = discord.Game(f"{self.PREFIX}help | Bot à multiple fonctions")
+        await self.change_presence(status=discord.Status.online, activity=game)
+
+    async def help(self, ctx: commands.Context, module: str = None, command: str = None):
+        if module is None:
+            embed = discord.Embed(
+                title="Voici tous les modules disponibles avec leurs commandes !",
+                color=discord.Color.orange()
+            )
+            cogs_help_count = self.MAX_COGS_HELP
+            if cogs_help_count > len(self.cogs):
+                cogs_help_count = len(self.cogs)
+
+            start = 0
+            bonus = cogs_help_count
+            last_start = 0
+            len_cogs = len(self.cogs)
+
+            def check(reaction: discord.Reaction, user: discord.User) -> bool:
+                return user == ctx.author and reaction.emoji in ("⬅️", "➡️")
+
+            while True:
+                embed.clear_fields()
+                embed = self.generate_help_embed(start, start + bonus, embed)
+                embed_msg = await ctx.send(embed=embed)
+
+                await embed_msg.add_reaction("⬅️")
+                await embed_msg.add_reaction("➡️")
+                try:
+                    reaction, user = await self.wait_for("reaction_add", timeout=5, check=check)
+                    if reaction.emoji == "⬅️":
+                        start -= bonus
+                        if start < start + bonus < bonus:
+                            start = 0
+                            if last_start == start:
+                                break
+
+                        last_start = start
+                    else:
+                        start += bonus
+                        if start < start + bonus > len_cogs:
+                            start = len_cogs - 1
+                            if last_start == start:
+                                break
+
+                        last_start = start
+
+                    continue
+                except asyncio.TimeoutError as e:
+                    logger.debug(e)
+                    return
+        else:
+            cog = self.get_cog(module)
+            if cog is None or getattr(cog, "hidden", False):
+                await ctx.send(f"Ce module est introuvable. Veuillez en renseigner un ou taper '{self.PREFIX}help'")
+                return
+
+            cog_help_func = getattr(cog, "help", False)
+            if not cog_help_func:
+                await ctx.send("Ce module ne contient pas encore d'aide. :frowning:")
+                return
+
+            try:
+                await cog_help_func(ctx, command)
+            except Exception as e:
+                logger.debug(f"Exception : {e}")
+                await ctx.send("Une erreur est survenue en essayant d'afficher l'aide du module. :rage:")
+
+    def generate_help_embed(self, start: int, end: int, embed: discord.Embed = None):
+        if embed is None:
+            embed = discord.Embed()
+
+        cogs = tuple(self.cogs.items())
+        for cog_name, cog in cogs[start: end]:
+            if getattr(cog, "hidden", False):
+                continue
+
+            cog_commands = ""
+            cmds = cog.get_commands()
+            cmds_help_count = self.MAX_COMMANDS_HELP
+            if cmds_help_count > len(cmds):
+                cmds_help_count = len(cmds)
+
+            for cmd in cmds[: cmds_help_count]:
+                if cmd.hidden:
+                    continue
+
+                cog_commands += f"- *{cmd.name}*\n"
+
+            embed.add_field(name=f"**{cog_name}** :", value=cog_commands)
+
+        return embed
 
     def process_args(self):
         args = self.args
@@ -30,8 +137,12 @@ class Bot(commands.Bot):
 
         self.TOKEN = args.get("token") or os.getenv("TOKEN")
         self.PREFIX = args.get("prefix") or os.getenv("PREFIX")
+
+        logs_handler = logging.StreamHandler(self.logs)
+        stdout_handler = logging.StreamHandler(sys.stdout)
         self.DEBUG = args.get("debug") or __debug__
-        logging.basicConfig(level=logging.DEBUG if self.DEBUG else logging.INFO, format=self.FORMAT)
+        level = logging.DEBUG if self.DEBUG else logging.INFO
+        logging.basicConfig(level=level, format=self.FORMAT)
 
     def load_cogs(self):
         cogs_directory = self.COGS_DIRECTORY
@@ -71,9 +182,3 @@ class Bot(commands.Bot):
     def run_bot(self):
         self.load_cogs()
         self.run(self.TOKEN)
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        logger.info("Bot ready to use.")
-        game = discord.Game(f"{self.PREFIX}help | Bot à multiple fonctions")
-        await self.change_presence(status=discord.Status.online, activity=game)
